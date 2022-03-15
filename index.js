ed351c1d78f95d2b5c14b28e9407086a5237dca2
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-15 16:01:39 -0700

    add middleware and start server console log

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || 8000);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded( {extended: true} ));
+
+// Routes
+
+// Start Server
+app.listen(app.get('port'), () => {
+    console.log(`✅ PORT: ${app.get('port')} 🌟`);
+});
