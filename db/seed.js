9d3acce1408871216f4cc0399a1905bb68b8e074
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-17 15:53:52 -0700

    add to seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,21 @@
 // require model
+const Order = require('../models/Order');
 
 // require seeds.json
+const orderSeeds = require('./seed.json');
 
 // Empty database and reseed
+Order.deleteMany({})
+    .then(()=>{
+        console.log('Deleted all orders!');
+    })
+    .then(()=>{
+        return Order.insertMany(orderSeeds);
+    })
+    .then((newOrders)=>{
+        console.log('Creaded new Orders!', newOrders);
+    })
+    .catch(console.error)
+    .finally(()=>{
+        process.exit();
+    });
