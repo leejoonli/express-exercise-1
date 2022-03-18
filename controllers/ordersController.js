23af17875ce94c11a153bb98e94ae5ad26c75188
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-18 16:35:25 -0700

    add post route

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -25,9 +25,12 @@ router.get('/:id', async (req, res, next) => {
     try {
         // get single order
         const order = await Order.findById(req.params.id);
+        // if found
         if(order) {
+            // send response in JSON
             res.json(order);
         } else {
+            // else send status 404 not found
             res.sendStatus(404);
         }
     } catch(err) {
@@ -36,6 +39,16 @@ router.get('/:id', async (req, res, next) => {
 });
 
 // POST
+router.post('/', async (req, res, next) => {
+    try {
+        // find order to update
+        const updatedOrder = await Order.findByIdAndUpdate(req.params.id, req.body, {new: true, overwrite: true});
+        // send response in JSON
+        res.json(updatedOrder);
+    } catch(err) {
+        next(err);
+    }
+});
 
 // PUT
 
